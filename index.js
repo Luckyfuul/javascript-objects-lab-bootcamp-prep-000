4989284e752ffd211b9dcfbed01b157103189356
TJ Zuvic <terrance@zuvmarketing.com>
2018-05-30 09:51:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var recipes = {prop: 1}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  recipes[key] = value
+  return recipes
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+     object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
