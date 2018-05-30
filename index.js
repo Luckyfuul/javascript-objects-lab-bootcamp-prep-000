b82defeb856086023b0e1df1712c8109d6a443ab
TJ Zuvic <terrance@zuvmarketing.com>
2018-05-30 09:59:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 function deleteFromObjectByKey(object, key){
  return delete object.key;
 }
-destructivelyDeleteFromObjectByKey(object, key)
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key;
+  return object
+}
